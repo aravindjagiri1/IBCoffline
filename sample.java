902d951bfd2e346bb004517de28bc54e3dccc4c3
aravindjagiri1 <virushka2226@gmail.com>
2025-05-01 12:27:52 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,11 @@
+class StudentDetails
+{
+	public static void main (String[] args)
+{
+	String name="Aravind";
+	int rollNo=1;
+	int marks=100;
+	String course="developer";
+      System.out.println(name);
+}
+}
