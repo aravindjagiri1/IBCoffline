b9439b346ba872de9b7e62e22238b4802133cbb5
aravindjagiri <aravindjagiri1@gamil.com>
2025-05-05 09:27:24 +0530

    practice file

---
+++ student.java
@@ -0,0 +1,12 @@
+class StudentDetails
+{
+	public static void main (String[] args)
+{
+	String name="Aravind";
+	int rollNo=1;
+	int marks=100;
+	String course="developer";
+      System.out.println(marks);
+}
+}
+
